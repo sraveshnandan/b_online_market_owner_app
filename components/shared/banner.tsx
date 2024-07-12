350f5fe9d661bdb44fa884b92dffb00fae398942
sraveshnandan <kumarsravesh39@gmail.com>
2024-07-12 11:12:58 +0530

    package installation

--- components/shared/banner.tsx
+++ components/shared/banner.tsx
@@ -1,16 +1,96 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { tintColorLight } from "@/constants";
+import React, { useEffect, useRef, useState } from "react";
+import {
+    View,
+    StyleSheet,
+    ScrollView,
+    Dimensions,
+    Text,
+    Image,
+} from "react-native";
 
-type Props = {}
+const Slider = ({ images }: { images: string[] }) => {
+    const { width } = Dimensions.get("window");
+    const height = width * 0.7;
 
-const banner = (props: Props) => {
+    const [active, setActive] = useState(0);
+    const scrollViewRef = useRef<ScrollView | null>(null);
+
+    const onScrollChange = ({ nativeEvent }: { nativeEvent: any }) => {
+        const slide = Math.ceil(
+            nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width
+        );
+        if (slide !== active) {
+            setActive(slide);
+        }
+    };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (active < images.length - 1) {
+                scrollViewRef.current?.scrollTo({
+                    x: (active + 1) * width,
+                    animated: true,
+                });
+                setActive(active + 1);
+            } else {
+                scrollViewRef.current?.scrollTo({
+                    x: 0,
+                    animated: true,
+                });
+                setActive(0);
+            }
+        }, 1500);
+
+        return () => clearInterval(interval);
+    }, [active, images.length]);
+
     return (
         <View>
-            <Text>banner</Text>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                onScroll={onScrollChange}
+                showsHorizontalScrollIndicator={false}
+                style={{ width, height }}
+                ref={scrollViewRef}
+                scrollEventThrottle={16}
+            >
+                {!!images.length
+                    ? images.map((i, index) => (
+                        <Image
+                            style={{ width: width, height: height, resizeMode: "contain" }}
+                            key={index}
+                            source={{ uri: i }}
+                        />
+                    ))
+                    : (<View></View>)}
+            </ScrollView>
+            <View style={styles.pagination}>
+                {images.map((i, k) => (
+                    <Text key={k} style={k == active ? styles.activeDot : styles.dot}>
+                        •
+                    </Text>
+                ))}
+            </View>
         </View>
-    )
-}
+    );
+};
 
-export default banner
+const styles = StyleSheet.create({
+    pagination: {
+        flexDirection: "row",
+        position: "absolute",
+        bottom: -15,
+        alignSelf: "center",
+    },
+    dot: {
+        color: "#888",
+        fontSize: 40,
+    },
+    activeDot: {
+        color: tintColorLight,
+        fontSize: 40,
+    },
+});
 
-const styles = StyleSheet.create({})
+export default Slider;
